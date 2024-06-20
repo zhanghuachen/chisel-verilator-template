45a84498663a94d1a7451d0d313441313d7afc08
zhang <1213671667@qq.com>
2024-06-20 15:32:28 +0800

    modify mux41

--- src/main/scala/mux4_1/mux4_1.scala
+++ src/main/scala/mux4_1/mux4_1.scala
@@ -27,6 +27,6 @@ class mux41 extends Module {
     }
 }
 
-object mux4_1 extends App {
-    emitVerilog(new mux41, Array("--target-dir", "generated"))
-}
+// object mux4_1 extends App {
+//     emitVerilog(new mux41, Array("--target-dir", "generated"))
+// }
